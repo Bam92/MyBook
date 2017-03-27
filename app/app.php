b81087dc3e4cb91d41a660a8f3ff22d6d443a4b3
Bam92 <me@abelmbula.url.ph>
2017-03-28 00:12:03 +0200

    Iteration 6: integration of Bootstrap

--- app/app.php
+++ app/app.php
@@ -12,7 +12,8 @@ $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
-
+$app->register(new Silex\Provider\AssetServiceProvider(), array(
+	'assets.version' => 'v1'));
 
 // Register services.
 $app['dao.book'] = function ($app) {
